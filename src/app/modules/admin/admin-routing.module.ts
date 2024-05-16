d33fd0ea64ece9bf2e1b80d664bbfb5f20e9614a
SachaWildCode <sacha.loumachi@gmail.com>
2024-05-16 11:57:41 +0200

    hotfix

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AccountAdminComponent } from '../../src/app/models/admin/pages/account-admin/account-admin.component';
+import { AccountAdminComponent } from './pages/account-admin/account-admin.component';
 
 const routes: Routes = [{ path: 'home', component: AccountAdminComponent }];
 
--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AccountAdminComponent } from './pages/account-admin/account-admin.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccountAdminComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
